369854dd0a9e4be0f01f21aca2c048fd7df2f514
kolasyik <mykola.primachenko@gmail.com>
2023-09-04 21:45:35 +0000

    init commit

---
+++ server/src/auth/auth.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common';
+import { CreateAuthDto } from './dto/create-auth.dto';
+import { UpdateAuthDto } from './dto/update-auth.dto';
+import { UsersService } from '../users/users.service';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class AuthService {
+    constructor(
+        private usersService: UsersService,
+        private jwtService: JwtService,
+    ) {}
+    create(createAuthDto: CreateAuthDto) {
+        return 'This action adds a new auth';
+    }
+
+    findAll() {
+        return `This action returns all auth`;
+    }
+
+    findOne(id: number) {
+        return `This action returns a #${id} auth`;
+    }
+
+    update(id: number, updateAuthDto: UpdateAuthDto) {
+        return `This action updates a #${id} auth`;
+    }
+
+    remove(id: number) {
+        return `This action removes a #${id} auth`;
+    }
+}
