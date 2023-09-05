a3c40a9817001633af486c1dc09cb99127fe374f
kolasyik <mykola.primachenko@gmail.com>
2023-09-05 20:17:03 +0000

    change socket url

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = 'http://localhost';
+const URL = 'https://crocodile-mykolaprimachenko.b4a.run/';
 // const URL = 'https://wild-jade-crayfish-kit.cyclic.app';
 
 export const socket = io(URL);
